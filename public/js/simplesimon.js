d22d9a192e7980fe5ed3cc7667f6d68497b8e5ea
Paula C Schuler <paulasatx@gmail.com>
2015-05-26 19:06:02 -0500

    Compare function successful at index 0 of arrays.

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -1,96 +1,112 @@
 // Exercise 05.5 Simple Simon Game, JavaScript version 
 // To Be Completed
 
-
-buttons = document.getElementsByClassName("buttons");
 var simonSequence = [];
 var userSequence = [];
-var score = 1;
+var clicks = "";
 
 var btn0 = document.getElementById("purple");
 var btn1 = document.getElementById("blue");
 var btn2 = document.getElementById("green");
 var btn3 = document.getElementById("pink"); 
 var start = document.getElementById("start");
-
-
+var purple = document.getElementById("purple");
+var blue = document.getElementById("blue");
+var green = document.getElementById("green");
+var pink = document.getElementById("pink");
+var colors = ["purple", "blue", "green", "pink"];
+var	buttons = [btn0, btn1, btn2, btn3];
 
 // works
 var getSimonSelection = function (){
-	var simonSelection = Math.floor(Math.random() * 4);
+	var simonSelection = colors[Math.floor(Math.random() * 4)];
 	console.log(simonSelection + " is Simons new number");
-
+	// var simonSequence = [];
 	simonSequence.push(simonSelection);
 	console.log(simonSequence + " is simons array");
-
-	score = simonSequence.length;
 	
-	var timeoutId = setTimeout (function(){
-		// document.getElementById("roundCounter").innerHTML = "Score: " + round;
-	},500);
 	playSimonSequence(simonSequence);
 }
 
 
 function playSimonSequence(simonSequence){
-	
-	var simonsTurn = setInterval(function(){
 
-		
-		if (simonSequence == "0"){
-			buttonToFlash = btn0;
-			buttonFlash(buttonToFlash);
-		} else if (simonSequence == "1"){
-			buttonToFlash = btn1;
-			buttonFlash(buttonToFlash);
-		} else if (simonSequence == "2"){
-			buttonToFlash = btn2;
-			buttonFlash(buttonToFlash);
-		} else if (simonSequence == "3"){
-			buttonToFlash = btn3;
-			buttonFlash(buttonToFlash);
-		} else {
-			clearInterval(simonsTurn);
-		}
-		
-	}, 500);
+	if (simonSequence == "purple"){
+		buttonToFlash = btn0;
+		buttonFlash(buttonToFlash);
+	} else if (simonSequence == "blue"){
+		buttonToFlash = btn1;
+		buttonFlash(buttonToFlash);
+	} else if (simonSequence == "green"){
+		buttonToFlash = btn2;
+		buttonFlash(buttonToFlash);
+	} else if (simonSequence == "pink"){
+		buttonToFlash = btn3;
+		buttonFlash(buttonToFlash);
+	} else {
+	}
 }
 
-// works
+
+
+
+
+// works for user
 function buttonFlash (buttonToFlash){
-	console.log("in buttonToFlash");
+	console.log("in Flash");
 	buttonToFlash.style.opacity = "1";
 	var timeoutId = setTimeout (function(){
 		buttonToFlash.style.opacity = "0.5"
-	}, 500);
+	}, 500 );
 }
-
-
 
 // Listeners call userPlays
 // Adds user choice to array and calls function to flash button
 // works
 var userPlays = function(){
 	var userPick = this.value;
-	console.log(userPick + " is users pick");
-	userSequence.push(userPick);
-
-
-	if (this.value == "0"){
-		buttonFlash(btn0);
-	} else if (this.value == "1"){
-		buttonFlash(btn1);
-	} else if (this.value == "2"){
-		buttonFlash(btn2);
-	} else if (this.value == "3"){
-		buttonFlash(btn3);
-	} 
+	console.log(userPick);
+	console.log("is userPick");
 	
+	if (userPick == "0"){
+		buttonFlash(btn0);
+		userSequence.push("purple");
+		console.log(userSequence);
+	} else if (userPick == "1"){
+		buttonFlash(btn1);
+		userSequence.push("blue");
+		console.log(userSequence);
+	} else if (userPick == "2"){
+		buttonFlash(btn2);
+		userSequence.push("green");
+		console.log(userSequence);
+	} else if (userPick == "3"){
+		buttonFlash(btn3);
+		userSequence.push("pink");
+		console.log(userSequence);
+	} 
 	console.log(userSequence + " is user array in userPlays");
-	console.log(simonSequence + "simonSequence in userPlays");
-	// compare(userSequence);
+	console.log(simonSequence + " is simonSequence in userPlays");
+	compare();
 
 }		
+
+
+	var compare = function() {
+		console.log(simonSequence);
+		console.log(userSequence);
+		if (userSequence.length == 1) {
+			if (userSequence[0] == simonSequence[0]) {
+				console.log("Match in compare");
+				getSimonSelection(simonSequence);
+			}
+		}
+			console.log(simonSequence + " is simonSequence in compare loop");
+			console.log(userSequence + " is userSequence in compare loop");
+	}
+	
+	
+	
 
 
 
