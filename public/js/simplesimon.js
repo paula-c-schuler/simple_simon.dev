606d8b6939bd8b6bbedafd326c5847d9164f1a51
Paula C Schuler <paulasatx@gmail.com>
2015-05-29 16:33:28 -0500

    Fixed the timing issue when simon played the same color twice.
    The color would not appear to play twice, just long for one time.

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -3,7 +3,7 @@
 
 var simonSequence = [];
 var userSequence = [];
-var clicks = "";
+var click;
 
 var btn0 = document.getElementById("purple");
 var btn1 = document.getElementById("blue");
@@ -15,47 +15,55 @@ var blue = document.getElementById("blue");
 var green = document.getElementById("green");
 var pink = document.getElementById("pink");
 var colors = ["purple", "blue", "green", "pink"];
-var	buttons = [btn0, btn1, btn2, btn3];
+var clicksDisplay = document.getElementById("clicks");
 
 // works
-var getSimonSelection = function (){
+var getSimonSelection = function () {
 	var simonSelection = colors[Math.floor(Math.random() * 4)];
-	console.log(simonSelection + " is Simons new number");
-	// var simonSequence = [];
+	// console.log(simonSelection + " is Simons new number");
+
 	simonSequence.push(simonSelection);
-	console.log(simonSequence + " is simons array");
-	
-	playSimonSequence(simonSequence);
+	// console.log(simonSequence + " is simons array");
+	i = 0;
+	var timeoutId = setTimeout (function() {
+		playSimonSequence(simonSequence);
+	}, 500 );
 }
 
-
-function playSimonSequence(simonSequence){
-
-	if (simonSequence == "purple"){
-		buttonToFlash = btn0;
-		buttonFlash(buttonToFlash);
-	} else if (simonSequence == "blue"){
-		buttonToFlash = btn1;
-		buttonFlash(buttonToFlash);
-	} else if (simonSequence == "green"){
-		buttonToFlash = btn2;
-		buttonFlash(buttonToFlash);
-	} else if (simonSequence == "pink"){
-		buttonToFlash = btn3;
-		buttonFlash(buttonToFlash);
+// works
+function playSimonSequence(simonSequence) {
+	// console.log(simonSequence);
+	// console.log(simonSequence.length);
+	if (i < simonSequence.length) {
+		if (simonSequence[i] == "purple") {
+			buttonToFlash = btn0;
+			buttonFlash(buttonToFlash);		
+		} else if (simonSequence[i] == "blue") {
+			buttonToFlash = btn1;
+			buttonFlash(buttonToFlash);
+		} else if (simonSequence[i] == "green") {
+			buttonToFlash = btn2;
+			buttonFlash(buttonToFlash);		
+		} else if (simonSequence[i] == "pink") {
+			buttonToFlash = btn3;
+			buttonFlash(buttonToFlash);		
+		} 
+		i++;
+		var timeoutId = setTimeout (function() {
+			playSimonSequence(simonSequence);
+			userSequence = [];
+		}, 1000 );
 	} else {
+		click = 0;
 	}
 }
 
 
-
-
-
-// works for user
-function buttonFlash (buttonToFlash){
-	console.log("in Flash");
+// works for user and simon
+function buttonFlash (buttonToFlash) {
+	// console.log("in Flash");
 	buttonToFlash.style.opacity = "1";
-	var timeoutId = setTimeout (function(){
+	var timeoutId = setTimeout (function() {
 		buttonToFlash.style.opacity = "0.5"
 	}, 500 );
 }
@@ -63,52 +71,51 @@ function buttonFlash (buttonToFlash){
 // Listeners call userPlays
 // Adds user choice to array and calls function to flash button
 // works
-var userPlays = function(){
+var userPlays = function() {
 	var userPick = this.value;
-	console.log(userPick);
-	console.log("is userPick");
+	// console.log(userPick + "is userPick");
 	
-	if (userPick == "0"){
+	if (userPick == "0") {
 		buttonFlash(btn0);
+		userPick = "purple";
 		userSequence.push("purple");
-		console.log(userSequence);
-	} else if (userPick == "1"){
+		// console.log(userSequence);
+	} else if (userPick == "1") {
 		buttonFlash(btn1);
+		userPick = "blue";
 		userSequence.push("blue");
-		console.log(userSequence);
-	} else if (userPick == "2"){
+		// console.log(userSequence);
+	} else if (userPick == "2") {
 		buttonFlash(btn2);
+		userPick = "green";
 		userSequence.push("green");
-		console.log(userSequence);
-	} else if (userPick == "3"){
+		// console.log(userSequence);
+	} else if (userPick == "3") {
 		buttonFlash(btn3);
+		userPick = "pink";
 		userSequence.push("pink");
-		console.log(userSequence);
+		// console.log(userSequence);
 	} 
 	console.log(userSequence + " is user array in userPlays");
 	console.log(simonSequence + " is simonSequence in userPlays");
-	compare();
-
+	compare(simonSequence, userPick, click);
 }		
 
-
-	var compare = function() {
-		console.log(simonSequence);
-		console.log(userSequence);
-		if (userSequence.length == 1) {
-			if (userSequence[0] == simonSequence[0]) {
-				console.log("Match in compare");
-				getSimonSelection(simonSequence);
-			}
-		}
-			console.log(simonSequence + " is simonSequence in compare loop");
-			console.log(userSequence + " is userSequence in compare loop");
+// Should get a match or no match for each user button push
+// Only getting one match so far 
+var compare = function(simonSequence, userPick, click) {
+	console.log(simonSequence + " is in compare");
+	console.log(click + " is click");
+	if (userSequence[click] == simonSequence[click]) {
+		console.log("Match in compare");
+		click++;
+		if (userSequence.length == simonSequence.length)
+			console.log("Full match.");
+	} else {
+		alert("Game Over");
 	}
+}
 	
-	
-	
-
-
 
 btn0.addEventListener("click", userPlays, false);
 btn1.addEventListener("click", userPlays, false);
